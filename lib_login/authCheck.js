811b55f8ea142595d827ccd907180d94f218ae3d
hyeonseo2006 <hyeonseoalex@gmail.com>
2023-10-23 12:12:26 -0700

    버튼 수정 및 폰트 적용

--- lib_login/authCheck.js
+++ lib_login/authCheck.js
@@ -5,7 +5,7 @@ module.exports = {
     statusUI: function (request, response) {
       var authStatusUI = '<a href="/auth/login">로그인</a>';
       if (this.isOwner(request, response)) {
-        authStatusUI = `${request.session.nickname}님 VGMC청소년부에 오신걸 환영합니다`;
+        authStatusUI = `${request.session.nickname}님`;
       }
       return authStatusUI;
     }
